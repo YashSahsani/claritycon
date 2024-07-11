415c9a71651af5ff6dafd068e3970b675d2a2be2
YashSahsani <yashsahsani5@gmail.com>
2024-07-11 14:19:54 -0400

    feat: sidebar created

---
+++ constants/index.ts
@@ -0,0 +1,30 @@
+export const sidebarLinks = [
+    {
+    label: "Home",
+    imageUrl: "/icons/home.svg",
+    route: "/",
+
+},
+{
+    label: "Upcoming",
+    imageUrl: "/icons/upcoming.svg",
+    route: "/upcoming",
+
+},{
+    label: "Previous",
+    imageUrl: "/icons/previous.svg",
+    route: "/previous",
+
+},
+{
+    label: "Recordings",
+    imageUrl: "/icons/Video.svg",
+    route: "/recordings",
+
+},
+{
+    label: "Personal Room",
+    imageUrl: "/icons/add-personal.svg",
+    route: "/personal-room",
+
+}]
