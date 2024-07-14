53fc82acf680f78f6a43e201bb87dd04e95ec13a
YashSahsani <yashsahsani5@gmail.com>
2024-07-14 16:33:53 -0400

    feat: mobilnav bar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
   const pathName = usePathname();
   return (
     <section className="sticky left-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:2-[264]px">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
             pathName === link.route || pathName.startsWith(link.route);
